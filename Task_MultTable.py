5d0d2c28277fa1c4016f918f2087540472630912
Александр <60809442+Ubivakus@users.noreply.github.com>
2022-10-30 14:47:06 +0300

    Add files via upload
    2 step
    Create function for table

--- Task_MultTable.py
+++ Task_MultTable.py
@@ -1,9 +1,24 @@
-# def multiply(list1, list2):   
-#   for i in range(len(list2)):
-#     for j in range(len(list1)):
-#       result = list2[i] * list1[j]
-#       print(result)
-      
+def multiply(list_row, list_col):
+  for k in range(len(list_row)):
+    if list_row[k] <= 10:
+      print('', list_row[k], sep='      ', end='')
+    else:
+      print('', list_row[k], sep='     ', end='')
+  for i in range(len(list_col)):
+    print()
+    if list_col[i] < 10:
+      print(list_col[i], end='     ')
+    else:
+      print(list_col[i], end='    ')
+    for j in range(len(list_row)):
+      result = list_col[i] * list_row[j]
+      if result < 10:
+        print(result, end='      ')
+      elif 9 < result < 100:
+        print(result, end='     ')
+      else:
+        print(result, end='    ')
+
 
 numbers_row = input("Введите первый диапазон через пробел: ")
 list_row = numbers_row.split(' ')
@@ -12,7 +27,5 @@ list_row = [i for i in range(int(list_row[0]), int(list_row[1]) + 1)]
 numbers_col = input("Введите второй диапазон через пробел: ")
 list_col = numbers_col.split(' ')
 list_col = [i for i in range(int(list_col[0]), int(list_col[1]) + 1)]
+
 multiply(list_row, list_col)
-# # print('', list1[0], list1[1], sep='      ')
-
-
